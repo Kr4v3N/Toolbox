ae9ba937dba82a9d64e9b232c59696f7f328987e
Kr4v3n <fafachen37@gmail.com>
2022-02-25 02:24:04 +0100

    xssscan

---
+++ XSS/XSSScan.py
@@ -0,0 +1,14 @@
+import requests
+import sys
+
+url = sys.argv[1]
+payloads = ['<script>alert(1);</script>']
+
+for payload in payloads:
+    req = requests.post(url + payload)
+    if payload in req.text:
+        print("Le paramétre est vulnérable \r\n")
+        print(("Payload utilisé : " + payload))
+        print(req.text)
+
+        break
