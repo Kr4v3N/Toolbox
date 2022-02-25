12a51568f70dbb975a9eb8728ac50ae889629b3f
Kr4v3n <fafachen37@gmail.com>
2022-02-25 02:22:17 +0100

    porscanner

--- portScanner.py
+++ portScanner.py
@@ -1,34 +1,38 @@
 import socket
-import subprocess
 import sys
 from datetime import datetime
+import pyfiglet
 
-subprocess.call('clear', shell=True)
+
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
 
 remoteServerIP = input('Entrez l\'IP d\'une machine à scanner : ')
 
 print('-' * 60)
-print('Lancement du scan des ports de la machine' + remoteServerIP)
+print('Lancement du scan des ports de la machine  ' + remoteServerIP)
 print('-' * 60)
 
 t1 = datetime.now()
 
 try:
-    for port in range(1,1025):
+    for port in range(1, 1025):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
             print('Port {}:   Ouvert'.format(port))
         sock.close()
+    t2 = datetime.now()
+
+    total = t2 - t1
+    print('Scan complété en: {}'.format(str(total)))
 
 except KeyboardInterrupt:
-    print('Vous avez appuyez sur CTRL+C')
+    print('Vous avez appuyez sur CTRL+C.')
+    sys.exit()
+except socket.gaierror:
+    print("\n Le nom d'hôte n'a pas pu être résolu.")
     sys.exit()
 except socket.error:
     print('Impossible de se connecter au serveur.')
     sys.exit()
-
-t2 = datetime.now()
-
-total = t2 -t1
-print('Scan complété en: {}'.format(str(total)))
