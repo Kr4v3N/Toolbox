12ae32dbbcc26310796b0a98841efe9509f47342
Fayçal Chena <fafachen37@gmail.com>
2023-02-12 21:50:24 +0100

    Refactorings XSS script

--- XSS/XSSScan.py
+++ XSS/XSSScan.py
@@ -1,14 +1,43 @@
 import requests
-import sys
+import urllib
+from bs4 import BeautifulSoup
+import colorama
 
-url = sys.argv[1]
-payloads = ['<script>alert(document.cookie);</script>']
+def perform_xss_scan(url, payloads_file):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status code {response.status_code}")
 
-for payload in payloads:
-    req = requests.post(url + payload)
-    if payload in req.text:
-        print("Le paramétre est vulnérable \r\n")
-        print(("Payload utilisé : " + payload))
-        print(req.text)
+    soup = BeautifulSoup(response.text, "html.parser")
+    input_fields = soup.find_all('input')
+    data = {}
 
-        break
+    colorama.init()
+    with open(payloads_file) as file:
+        for payload in file:
+            encoded_payload = urllib.parse.quote(payload)
+            for field in input_fields:
+                if field.has_attr('name'):
+                    if field['name'].lower() == "submit":
+                        data[field['name']] = "submit"
+                    else:
+                        data[field['name']] = encoded_payload
+                        response = requests.post(url, data=data)
+                        if response.status_code != 200:
+                            raise Exception(f"Request failed with status code {response.status_code}")
+                        elif payload in response.text or encoded_payload in response.text:
+                            print(f'\033[32mPayload "{payload}" returned in input fields\033[0m')
+                        else:
+                            soup = BeautifulSoup(response.text, "html.parser")
+                            if soup.body and payload in soup.body.get_text() or encoded_payload in soup.body.get_text():
+                                print(f'\033[32mPayload "{payload}" returned in body\033[0m')
+                        data = {}
+    colorama.deinit()
+
+try:
+    url = 'http://testphp.vulnweb.com/search.php?test=query'
+    payloads_file = '/home/kr4v3n/Documents/Projets divers/PYTHON DJANGO/Toolbox/XSS/XSS-payloads.txt'
+
+    perform_xss_scan(url, payloads_file)
+except Exception as e:
+    print(f"An error occured: {e}")
